de5b9cc7fe5bf03c026a6cee6d0e111afff07525
Evelina Kirichkova <writerometr@gmail.com>
2021-06-10 23:42:51 +0400

    5 модуль 1-3 задачи

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
-  // ваш код...
+let buttonHide = document.querySelector('.toggle-text-button');
+let textinf = document.getElementById('text');
+buttonHide.onclick = function () {
+  if (textinf.hidden === false){
+    textinf.hidden = true;
+  }
+  else {textinf.hidden = false} 
+}
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,42 @@
 function initCarousel() {
-  // ваш код...
+  /*Обозначения*/
+  let rightBtn = document.querySelector('.carousel__arrow_right');
+  let leftBtn = document.querySelector('.carousel__arrow_left');
+  let inner = document.querySelector('.carousel__inner');
+  let slideWidth = inner.children[0].offsetWidth;
+  
+ inner.slideNumbers = 1;
+ leftBtn.style.display = 'none';
+
+  /*Работающая правая кнопка*/
+
+  rightBtn.onclick = function () {
+
+
+    if (inner.slideNumbers === 3) {
+      rightBtn.style.display = 'none';
+    }
+    if (inner.slideNumbers === 1) {
+      leftBtn.style.display = '';
+    }
+  
+
+    inner.style.transform = 'translateX(' + (-slideWidth * (inner.slideNumbers)) + 'px'+ ')';
+    inner.slideNumbers +=1;
+
+  }
+  /*Работающая левая кнопка*/
+  leftBtn.onclick = function () {
+
+    if (inner.slideNumbers === 2) {
+      leftBtn.style.display = 'none';
+    }
+    if (inner.slideNumbers === 4){
+      rightBtn.style.display = '';
+    }
+
+
+    inner.style.transform = 'translateX(' + (-slideWidth * (inner.slideNumbers - 2))+ 'px' + ')';
+    inner.slideNumbers -=1;
+  }
 }
