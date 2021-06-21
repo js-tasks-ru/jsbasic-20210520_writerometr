4009f3d9db7629d607bf9726da9a8b618fa0ab65
Evelina Kirichkova <writerometr@gmail.com>
2021-06-21 15:21:23 +0400

    сделаны: Таблица с удаляемыми строками, Карточка товара, Карусель (Занятие 6)

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,39 @@
  */
 export default class UserTable {
   constructor(rows) {
+    let body = document.querySelector('body');
+    
+    let tbl = body.appendChild(document.createElement('table'));
+     this.elem = tbl;
+    
+    let tblHead = tbl.appendChild(document.createElement('thead'));
+    let tblBody = tbl.appendChild(document.createElement('tbody'));
+    let tblHeadTr = tblHead.appendChild(document.createElement('tr'));
+
+    tblHeadTr.appendChild(document.createElement('th')).textContent = 'Имя';
+    tblHeadTr.appendChild(document.createElement('th')).textContent = 'Возраст';
+    tblHeadTr.appendChild(document.createElement('th')).textContent = 'Зарплата';
+    tblHeadTr.appendChild(document.createElement('th')).textContent = 'Город';
+
+    for (let i = 0; i < rows.length; i++){
+  this.name = rows[i].name;
+   this.age = rows[i].age;
+   this.salary = rows[i].salary;
+   this.city = rows[i].city;
+   
+  
+
+   
+   let tblBodyTr = tblBody.appendChild(document.createElement('tr'));
+   tblBodyTr.appendChild(document.createElement('td')).textContent = this.name;
+   tblBodyTr.appendChild(document.createElement('td')).textContent = this.age;
+   tblBodyTr.appendChild(document.createElement('td')).textContent = this.salary;
+   tblBodyTr.appendChild(document.createElement('td')).textContent = this.city;
+   tblBodyTr.appendChild(document.createElement('td')).insertAdjacentHTML('afterbegin', "<button class='deleteButton'>X</button>");
+   tblBodyTr.lastChild.onclick = () => tblBodyTr.remove();
+    } 
+  let buttons = document.querySelectorAll('.deletedeleteButton');
+  console.log(buttons);
   }
+  
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -2,5 +2,50 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
   constructor(product) {
-  }
-}
+
+let body = document.querySelector('body');
+  /*let holdr = body.appendChild(document.createElement('div'));
+holdr.className="container";
+holdr.id="holder";*/
+   let crd = document.querySelector('body').appendChild(document.createElement('div'));
+   crd.className="card";
+   this.elem = crd;
+  
+   
+
+/*Создаем тело*/
+     crd.insertAdjacentHTML('afterbegin', '<div class="card__top"></div>');
+     crd.insertAdjacentHTML('beforeend', '<div class="card__body"></div>');
+/*Верх тела*/
+
+/*Создаем раздел с картинкой*/
+     document.querySelector('.card__top').insertAdjacentHTML('afterbegin', '<img class="card__image" alt="product" src=""></img>');
+      this.image = product.image;
+      document.querySelector('.card__image').src = "/assets/images/products/" + this.image;
+  /*Раздел цены*/
+      document.querySelector('.card__top').insertAdjacentHTML('beforeend', '<span class="card__price"></span>');
+      this.price = '€' + product.price.toFixed(2);
+      document.querySelector('.card__price').textContent = this.price;
+/*Низ тела*/
+
+    /*Название*/
+      document.querySelector('.card__body').insertAdjacentHTML('afterbegin', '<div class="card__title"></div>');
+      this.name = product.name;
+      document.querySelector('.card__title').textContent = this.name;
+      /*Кнопка добавить*/
+      document.querySelector('.card__body').insertAdjacentHTML('beforeend', '<button type="button" class="card__button"></button>');
+      let buttonAdd =  document.querySelector('.card__button');
+      buttonAdd.insertAdjacentHTML('afterbegin', '<img src="/assets/images/icons/plus-icon.svg" alt="icon"></img>');
+        /*Функционал кнопки*/
+        this.id = product.id;
+        let event = new CustomEvent('product-add', {
+          detail: this.id,
+          bubbles: true
+        })
+        buttonAdd.addEventListener("click", function(){
+          buttonAdd.dispatchEvent(event);
+        })
+      
+      }
+    }
+       
